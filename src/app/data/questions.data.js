ea84cf11fab3bf7e9f9a47dbd40ed8a58485cb8a
Ilazaar <ilyass.lazaar@gmail.com>
2016-10-02 00:33:59 +0100

    Man Questions

--- src/app/data/questions.data.js
+++ src/app/data/questions.data.js
@@ -5,91 +5,183 @@
     
         /*
          * Questions
+         * result :  Classic - Romantic - Dramatic - Natural - Creative - Elegant
          */
         male      : [
             {
-                text: ' male Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut lorem scelerisque ?',
-                image:'assets/images/quizzImage.png',
+                text: 'Select the attributes that best describe your personality: ',
                 responses:[
                     {
-                        value:"Response 1",
-                        flags:[0, 5,2]
+                        value:"Unconventional and Creative",
+                        flags:[0, 0,1, 0, 5, 0]
                     },
                     {
-                        value:"Response 2",
-                        flags:[5, 0, 3]
+                        value:"Romantic and Charismatic",
+                        flags:[0, 5, 1, 0, 1 ,0 ]
                     },
                     {
-                        value:"Response 3",
-                        flags:[0, 0, 5]
+                        value:"Friendly and Relaxed",
+                        flags:[2, 0, 0, 5, 0, 0]
                     },
                     {
-                        value:"Response 4",
-                        flags:[2, 3, 2]
+                        value:"Confidence and Self-assured",
+                        flags:[5, 0, 5, 0, 0, 1]
+                    },
+                    {
+                        value:"Perfectionist and Well-organised",
+                        flags:[2, 0, 0, 0, 0, 5]
                     }
                 ]
             },
             {
-                text: 'Question 2 yeeh?',
-                image:'assets/images/quizzImage.png',
+                text: 'How do you wear your hair?',
+                //image:'assets/images/quizzImage.png',
                 responses:[
                     {
-                        value:"Response 1",
-                        flags:[0, 5,2]
+                        value:"I have regular cuts but the only product I use is shampoo.",
+                        flags:[2, 0, 2, 3, 1, 0]
                     },
                     {
-                        value:"Response 2",
-                        flags:[5, 5,2]
+                        value:"I don't really care about it. I get it cut whenever I feel like it.",
+                        flags:[1, 0, 0, 3, 4, 0]
                     },
                     {
-                        value:"Response 3",
-                        flags:[1, 4,2]
+                        value:"I keep it clean and short. I use gel occasionally.",
+                        flags:[1, 2, 3, 1, 2]
                     },
                     {
-                        value:"Response 4",
-                        flags:[2, 3,2]
+                        value:"I keep it up with the latest styles. I have an assortment of products.",
+                        flags:[0, 2, 3, 0, 0, 4]
                     }
                 ]
             },
             {
-                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut lorem scelerisque ?',
+                text: 'What do you like to do on Saturdays?',
                 responses:[
                     {
-                        value:"Response 1",
-                        flags:[0, 5,2]
+                        value:"Some kind of activity: tennis, golf, sailing.",
+                        flags:[0, 1, 3, 2, 2, 3]
                     },
                     {
-                        value:"Response 2",
-                        flags:[5, 5,2]
+                        value:"Shop and go out.",
+                        flags:[2, 2, 1, 0, 0, 2]
                     },
                     {
-                        value:"Response 3",
-                        flags:[1, 4,2]
+                        value:"Hang out at home, play some video games, surf the net.",
+                        flags:[2, 1, 0, 3, 2, 0]
                     },
                     {
-                        value:"Response 4",
-                        flags:[2, 3,2]
+                        value:"Family time",
+                        flags:[2, 2, 2, 3, 0, 1]
                     }
                 ]
             },
             {
-                text: 'Question 2 yeeh?',
+                text: 'What do you wear when it is cold outside?',
                 responses:[
                     {
-                        value:"Response 1",
-                        flags:[0, 5,2]
+                        value:"An overcoat",
+                        flags:[3, 2, 4, 0, 0, 4]
                     },
                     {
-                        value:"Response 2",
-                        flags:[5, 5,2]
+                        value:"A leather jacket",
+                        flags:[1, 2, 3, 2, 1, 0]
                     },
                     {
-                        value:"Response 3",
-                        flags:[1, 4,2]
+                        value:"A sweatshirt",
+                        flags:[0, 0,0, 3, 3, 0]
                     },
                     {
-                        value:"Response 4",
-                        flags:[2, 3,2]
+                        value:"Whatever I grab first",
+                        flags:[0, 0, 0, 2, 3, 0]
+                    }
+                ]
+            },
+            {
+                text: 'What do you find most attractive in a woman?',
+                responses:[
+                    {
+                        value:"her sense of humor",
+                        flags:[0, 2, 0, 3, 2, 0]
+                    },
+                    {
+                        value:"her sense of style",
+                        flags:[2, 3, 2, 0, 1, 1]
+                    },
+                    {
+                        value:"how she gets along with the guys",
+                        flags:[2, 3,2, 1, 0, 2]
+                    },
+                    {
+                        value:"her elegance and class",
+                        flags:[2, 3, 2, 0, 1, 4]
+                    }
+                ]
+            },
+            {
+                text: 'What are your favorite colors?',
+                responses:[
+                    {
+                        value:"Neutrals, black, whites",
+                        flags:[4, 0, 2, 0, 0, 1]
+                    },
+                    {
+                        value:"Colors that represent authority",
+                        flags:[1, 1, 3, 0, 0, 2]
+                    },
+                    {
+                        value:"Simplistic colors (Gray, blue ..)",
+                        flags:[0, 0, 0, 4, 3, 0]
+                    },
+                    {
+                        value:"Pastels and soft colors, not too bold",
+                        flags:[0, 4, 0, 1, 2, 2]
+                    },
+                    {
+                        value:"Mixing all kinds of colors ",
+                        flags:[1, 1, 0, 3, 4, 1]
+                    }
+                ]
+            },
+            {
+                text: 'If you could buy one of these accessories, which would it be?',
+                responses:[
+                    {
+                        value:"A simple watch",
+                        flags:[3, 2, 1, 3, 0, 1]
+                    },
+                    {
+                        value:"Some kind of jewelry, a necklace or ring",
+                        flags:[2, 3, 3, 0, 2, 4]
+                    },
+                    {
+                        value:"Scarves",
+                        flags:[0, 2, 1, 1, 3, 3]
+                    },
+                    {
+                        value:"I don't like accesories",
+                        flags:[0, 0, 0, 3, 1, 0]
+                    }
+                ]
+            },
+            {
+                text: 'How much time do you spend deciding what to wear?',
+                responses:[
+                    {
+                        value:"5min",
+                        flags:[2, 0, 1, 4, 2, 0]
+                    },
+                    {
+                        value:"10min",
+                        flags:[3, 2, 2, 0, 3, 1]
+                    },
+                    {
+                        value:"20min",
+                        flags:[2, 3, 2, 0, 2, 2]
+                    },
+                    {
+                        value:"30min ",
+                        flags:[1, 2, 1, 0, 0, 4]
                     }
                 ]
             }
